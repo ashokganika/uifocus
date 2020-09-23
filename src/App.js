aa512ce7661cee31696835f70755aaa6c00dd41f
Ashok <ashok.ag.ganika@gmail.com>
2020-09-23 17:47:11 +0545

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex">
+        <div className="flex-item">
+          <p>hello</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
